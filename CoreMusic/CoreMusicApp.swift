c508ea0c429142ae9061eb1b023a4e0fcdf79ada
Алексей Чёрный <avea05.alex@yandex.ru>
2026-02-04 01:30:47 +0300

    Initial Commit

---
+++ CoreMusic/CoreMusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreMusicApp.swift
+//  CoreMusic
+//
+//  Created by Алексей on 04.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct CoreMusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
